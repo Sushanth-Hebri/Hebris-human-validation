574a363ae43704e8f2deadd22cbef3cf8231fcae
Sushanth <117469085+Sushanth-Hebri@users.noreply.github.com>
2024-03-25 13:44:21 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -63,5 +63,7 @@ def predict():
     # Return predicted class as JSON response
     return jsonify({'predicted_class': predicted_class_name})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# Use Waitress server for production
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
